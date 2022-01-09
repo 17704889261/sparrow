86209171efd537b760058850fe4220fb9e501d61
17704889261 <17704889261@163.com>
2022-01-09 14:51:20 +0000

    Deploying to relase from  @ ab1d4dc15304fba6dc360f8cf34494e3dcf63fd7 🚀

---
+++ precache-manifest.6af49b5bb7e7321aa755384970fb4a06.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ce893671998ed0d6344b9e29efa019",
+    "url": "/sparrow/config.js"
+  },
+  {
+    "revision": "f04436445eeacf751ff7",
+    "url": "/sparrow/css/app.ac85c854.css"
+  },
+  {
+    "revision": "1d754fdbe89c14085d30",
+    "url": "/sparrow/css/chunk-2a718238.8c79b790.css"
+  },
+  {
+    "revision": "eb2313e5008e399fecb5",
+    "url": "/sparrow/css/chunk-53e66a82.92cb1e55.css"
+  },
+  {
+    "revision": "250d14031fee7d57519a",
+    "url": "/sparrow/css/chunk-5717300b.a29ff44c.css"
+  },
+  {
+    "revision": "b96d16622b16232bc2af",
+    "url": "/sparrow/css/chunk-79a03a7a.85d072ab.css"
+  },
+  {
+    "revision": "ce23362440d750c484fc",
+    "url": "/sparrow/css/chunk-elementui.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/sparrow/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/sparrow/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/sparrow/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a99cbebee7705c12f3bcc61ecc5bee98",
+    "url": "/sparrow/index.html"
+  },
+  {
+    "revision": "f04436445eeacf751ff7",
+    "url": "/sparrow/js/app.4cc34e1f.js"
+  },
+  {
+    "revision": "1d754fdbe89c14085d30",
+    "url": "/sparrow/js/chunk-2a718238.c37a85f1.js"
+  },
+  {
+    "revision": "6485b4540a8811ddf2d1",
+    "url": "/sparrow/js/chunk-2d0e19c3.634716a0.js"
+  },
+  {
+    "revision": "03ae44e0afef60e963d4",
+    "url": "/sparrow/js/chunk-2d0e95df.79b6d1a6.js"
+  },
+  {
+    "revision": "eb2313e5008e399fecb5",
+    "url": "/sparrow/js/chunk-53e66a82.142273c0.js"
+  },
+  {
+    "revision": "250d14031fee7d57519a",
+    "url": "/sparrow/js/chunk-5717300b.0f5e7f26.js"
+  },
+  {
+    "revision": "b96d16622b16232bc2af",
+    "url": "/sparrow/js/chunk-79a03a7a.5c0f6144.js"
+  },
+  {
+    "revision": "ce23362440d750c484fc",
+    "url": "/sparrow/js/chunk-elementui.12295a15.js"
+  },
+  {
+    "revision": "ca7a288fdd44d5ad3de6",
+    "url": "/sparrow/js/chunk-vue.f245de05.js"
+  },
+  {
+    "revision": "a2489a1cffb8aa69c91ce9ffe2f532a4",
+    "url": "/sparrow/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sparrow/robots.txt"
+  },
+  {
+    "revision": "c0ea6b6916c85367ae4a2530c7b744a1",
+    "url": "/sparrow/styles/common.css"
+  },
+  {
+    "revision": "78ef7447a14f7ca454893786cbe881a3",
+    "url": "/sparrow/styles/normailze.css"
+  }
+]);
