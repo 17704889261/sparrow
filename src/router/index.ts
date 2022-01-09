3d521c28b165a7375d8a694c0b11a42076e088ee
张庭玮 <17704889261@163.com>
2022-01-09 18:00:58 +0800

    Revert "Deploying to master from  @ 682e0544c4b850f4ee0fbff8ec8914b50e9818be 🚀"
    This reverts commit da1de5b13a4a372563edd331fddacb2f2402a331.

---
+++ src/router/index.ts
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import type { RouteConfig } from 'vue-router'
+import VueRouter from 'vue-router'
+
+import testModule from './modules/test'
+import sparrowModule from './modules/sparrow'
+
+Vue.use(VueRouter)
+
+export const routes: Array<RouteConfig> = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/login/index.vue')
+  },
+  ...sparrowModule,
+  {
+    path: '/',
+    name: 'Index',
+    redirect: '/test',
+    meta: {
+      title: 'Index'
+    }
+  },
+  ...testModule,
+  {
+    path: '/:pathMatch(.*)*',
+    name: '404',
+    component: () => import('@/views/404.vue')
+  }
+]
+
+export const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
