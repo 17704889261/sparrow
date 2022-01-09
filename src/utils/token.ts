2eccf3ba018a125ab3214f59697ca1d45aa7d252
17704889261 <17704889261@163.com>
2022-01-09 15:28:09 +0000

    Deploying to gh-pages from @ 17704889261/sparrow@2c36a489ca47725c1a185cac604958574a49fa62 🚀

--- src/utils/token.ts
+++
@@ -1,8 +0,0 @@
-export function getToken() {
-  return window.localStorage.getItem('token')
-}
-
-export function setToken(val: string) {
-  window.localStorage.setItem('token', val)
-  return val
-}
