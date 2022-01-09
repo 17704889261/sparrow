682e0544c4b850f4ee0fbff8ec8914b50e9818be
张庭玮 <17704889261@163.com>
2022-01-09 17:57:17 +0800

    Revert "Deploying to master from  @ f614284c4c83beecbcd97538614b820945d9c58b 🚀"
    This reverts commit cbfd48f1dbdb7a25bcb760fc5c9d124d79a73cef.

---
+++ src/views/bar/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <CanvasOne></CanvasOne>
+    <CanvasTwo></CanvasTwo>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+  import CanvasOne from './components/CanvasOne/index.vue'
+  import CanvasTwo from './components/CanvasTwo/index.vue'
+
+  const CANVAS_INFO = {
+    WIDTH: 1000,
+    HEIGHT: 1000
+  }
+
+  @Component({
+    components: {
+      CanvasOne,
+      CanvasTwo
+    }
+  })
+  export default class extends Vue {}
+</script>
+
+<style scoped>
+  canvas {
+    margin: 10px;
+  }
+</style>
